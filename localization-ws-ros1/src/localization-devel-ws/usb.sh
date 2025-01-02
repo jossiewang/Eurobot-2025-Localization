9da50343fbcce370ce36bf737dc880eecf60a9c1
jossiewang <jossiew621@gapp.nthu.edu.tw>
2025-01-02 15:24:55 +0800

    Fix: (Docker) safer usb script
    notes:
        or else might happen sth tragic
        like, if DEVICE-NAME is empty, it will change permission for the whole /dev/ directory

--- localization-ws-ros1/src/localization-devel-ws/usb.sh
+++ localization-ws-ros1/src/localization-devel-ws/usb.sh
@@ -38,16 +38,16 @@ fi
 # Get the device name from dmesg
 DEVICE_NAME=$(sudo dmesg | grep -oP 'cp210x converter now attached to \KttyUSB[0-9]+' | tail -1)
 
-if [ -z "$DEVICE_NAME" ]; then
-    echo "No cp210x device found."
-    exit 1
+# Validate the DEVICE_NAME to ensure it matches the expected pattern
+if [[ "$DEVICE_NAME" =~ ^ttyUSB[0-9]+$ ]]; then
+  DEVICE_PATH="/dev/$DEVICE_NAME"
+
+  # Change the permissions to read and write for all users
+  sudo chmod 666 $DEVICE_PATH
+
+  # Verify the permissions have been changed
+  echo "Updated permissions for $DEVICE_PATH:"
+  ls -l $DEVICE_PATH
+else
+  echo "Invalid device name: $DEVICE_NAME"
 fi
-
-DEVICE_PATH="/dev/$DEVICE_NAME"
-
-# Change the permissions to read and write for all users
-sudo chmod 666 $DEVICE_PATH
-
-# Verify the permissions have been changed
-echo "Updated permissions for $DEVICE_PATH:"
-ls -l $DEVICE_PATH
