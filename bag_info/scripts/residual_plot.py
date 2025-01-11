f66fdffff66a595481064ef311edde88601e678e
jossiewang <jossiew621@gapp.nthu.edu.tw>
2025-01-11 15:43:42 +0800

    Modified: (ResidualTest) time stamp

--- bag_info/scripts/residual_plot.py
+++ bag_info/scripts/residual_plot.py
@@ -14,7 +14,8 @@ with open('/home/user/bag_info/scripts/residual_data.csv', 'r') as file:
         data['3x_variance_theta'].append(float(row['3x_variance_theta']))
 
 # Create time axis
-time = range(len(data['residual_x']))
+frequency = 10  # Hz
+time = [i / frequency for i in range(len(data['residual_x']))]
 
 # Plot for x
 plt.figure(figsize=(10, 6))
@@ -22,7 +23,7 @@ plt.plot(time, data['residual_x'], label='Residual X')
 plt.plot(time, data['3x_variance_x'], label='Positive 3x Variance X', linestyle='--')
 plt.plot(time, [-y for y in data['3x_variance_x']], label='Negative 3x Variance X', linestyle='--')
 plt.axhline(0, color='black', linewidth=0.5)
-plt.xlabel('Time')
+plt.xlabel('Time (s)')
 plt.ylabel('X')
 plt.title('Residual X with 3x Variance')
 plt.legend()
@@ -34,7 +35,7 @@ plt.figure(figsize=(10, 6))
 plt.plot(time, data['residual_y'], label='Residual Y')
 plt.plot(time, data['3x_variance_y'], label='Positive 3x Variance Y', linestyle='--')
 plt.plot(time, [-y for y in data['3x_variance_y']], label='Negative 3x Variance Y', linestyle='--')
-plt.xlabel('Time')
+plt.xlabel('Time (s)')
 plt.ylabel('Y')
 plt.title('Residual Y with 3x Variance')
 plt.legend()
@@ -46,7 +47,7 @@ plt.figure(figsize=(10, 6))
 plt.plot(time, data['residual_theta'], label='Residual Theta')
 plt.plot(time, data['3x_variance_theta'], label='Positive 3x Variance Theta', linestyle='--')
 plt.plot(time, [-y for y in data['3x_variance_theta']], label='Negative 3x Variance Theta', linestyle='--')
-plt.xlabel('Time')
+plt.xlabel('Time (s)')
 plt.ylabel('Theta')
 plt.title('Residual Theta with 3x Variance')
 plt.legend()
