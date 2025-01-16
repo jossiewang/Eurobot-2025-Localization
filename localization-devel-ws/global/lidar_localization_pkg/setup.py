bfe436b13b5753f372fcaaf7471d5083aaee2c5e
jossiewang <jossiew621@gapp.nthu.edu.tw>
2025-01-16 15:29:43 +0800

    Fix: (BeaconProb) include .xml, .yml launch files
    notes:
            `(os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),`
            this only take the file end with .launch.py, so the file end with .launch will not be included
            The pattern [pxy][yma]* matches any file that has a .launch. followed by one of the characters p, x, or y and then one of the characters y, m, or a.

--- localization-devel-ws/global/lidar_localization_pkg/setup.py
+++ localization-devel-ws/global/lidar_localization_pkg/setup.py
@@ -1,6 +1,6 @@
 import os
 from glob import glob
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'lidar_localization_pkg'
 
@@ -8,7 +8,7 @@ setup(
     name=package_name,
     version='0.0.0',
     # Packages to export
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     # Files we want to install, specifically launch files
     data_files=[
         # Install marker file in the package index
@@ -17,7 +17,7 @@ setup(
         # Include our package.xml file
         (os.path.join('share', package_name), ['package.xml']),
         # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=[
         'setuptools',
