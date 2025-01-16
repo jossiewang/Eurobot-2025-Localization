515b0fd63ced7c3ace6670dc27062bad65ab4805
jossiewang <jossiew621@gapp.nthu.edu.tw>
2025-01-16 17:06:20 +0800

    Add: quaternion and euler conversion
    by: miffy

--- localization-devel-ws/global/lidar_localization_pkg/lidar_localization_pkg/lidar_member_function.py
+++ localization-devel-ws/global/lidar_localization_pkg/lidar_localization_pkg/lidar_member_function.py
@@ -47,6 +47,40 @@ class LidarLocalization(Node): # inherit from Node
         # use log to print what it get
         self.get_logger().debug('obs_raw: %s' % self.obs_raw)
 
+def quaternion_from_euler(ai, aj, ak):
+    ai /= 2.0
+    aj /= 2.0
+    ak /= 2.0
+    ci = math.cos(ai)
+    si = math.sin(ai)
+    cj = math.cos(aj)
+    sj = math.sin(aj)
+    ck = math.cos(ak)
+    sk = math.sin(ak)
+    cc = ci*ck
+    cs = ci*sk
+    sc = si*ck
+    ss = si*sk
+    q = np.empty((4, ))
+    q[0] = cj*sc - sj*cs
+    q[1] = cj*ss + sj*cc
+    q[2] = cj*cs - sj*sc
+    q[3] = cj*cc + sj*ss
+    return q
+    
+def euler_from_quaternion(self, x, y, z, w):
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+    return yaw_z # in radians
+
 def main(args=None):
     rclpy.init(args=args)
 
