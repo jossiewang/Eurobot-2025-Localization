fe2abf7ff5a09d22c3cade2ce8dc198c56ce1af5
jossiewang <jossiew621@gapp.nthu.edu.tw>
2025-02-18 22:59:06 +0800

    modified: (testBot) port and image

--- docker/testBot/OdomComm/yellow_run.sh
+++ docker/testBot/OdomComm/yellow_run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 ## 0. clean container within same group
-echo "=== [COMMUNICATION] Pull & Run ==="
-echo "[COMMUNICATION] Remove Containers ..."
-docker compose -p communication down --volumes --remove-orphans
+#echo "=== [COMMUNICATION] Pull & Run ==="
+#echo "[COMMUNICATION] Remove Containers ..."
+#docker compose -p communication down --volumes --remove-orphans
 
 ## 2. environment setup  
-export DISPLAY=:0
-xhost +local:docker
-cd docker
+#export DISPLAY=:0
+#xhost +local:docker
+#cd docker
 
 ## 3. build image
-echo "[COMMUNICATION] Pull Images ..."
-docker pull pomelo925/ttennis-humble:communication
+#echo "[COMMUNICATION] Pull Images ..."
+#docker pull pomelo925/ttennis-humble:communication
 
 ## 4. deployment
 echo "[COMMUNICATION] Deploying..."
-docker compose -p communication -f blue-compose.yml up
+docker compose -p communication -f docker/yellow-compose.yml up
