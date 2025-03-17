1bebd4056147721d89b4e1b86aa6bef93714da11
wintera1233 <zhen.cc.1207@gmail.com>
2025-03-17 22:02:49 +0800

    add: (ekf) covariance threshold params

--- localization-devel-ws/ekf/ekf/ekf_node.py
+++ localization-devel-ws/ekf/ekf/ekf_node.py
@@ -58,13 +58,8 @@ class EKFFootprintBroadcaster(Node):
         self.P[2, 2] = 0.003 # theta
         
         self.Q = np.eye(3) 
-        self.Q[0, 0] = 1e-5 # vx
-        self.Q[1, 1] = 1e-5 # vy
-        self.Q[2, 2] = 1e-8 # w
-
         self.R_gps = np.eye(3) * 1e-2
         self.R_camera = np.eye(3) * 1e-2
-        self.R_camera[2, 2] = 0.15
 
         self.last_odom_time = self.get_clock().now().nanoseconds / 1e9
         self.gps_time = self.get_clock().now().nanoseconds / 1e9
@@ -79,10 +74,25 @@ class EKFFootprintBroadcaster(Node):
         self.declare_parameter('robot_parent_frame_id', 'map')
         self.declare_parameter('robot_frame_id', 'base_footprint')
         self.declare_parameter('update_rate', 1)
-
+        self.declare_parameter('q_linear', 1e-3)
+        self.declare_parameter('q_angular', 1e-2)
+        self.declare_parameter('r_gps_angular', 1e-5)
+        self.declare_parameter('r_camera_linear', 1e-2)
+        self.declare_parameter('r_camera_angular', 0.15)
+        self.declare_parameter('r_gps_angular_threshold', 1e-2)
         self.parent_frame_id = self.get_parameter('robot_parent_frame_id').value
         self.child_frame_id = self.get_parameter('robot_frame_id').value
         self.rate = self.get_parameter('update_rate').value 
+        self.Q[0, 0] = self.get_parameter('q_linear').value
+        self.Q[1, 1] = self.get_parameter('q_linear').value
+        self.Q[2, 2] = self.get_parameter('q_angular').value
+        self.R_camera[0, 0] = self.get_parameter('r_camera_linear').value
+        self.R_camera[1, 1] = self.get_parameter('r_camera_linear').value
+        self.R_camera[2, 2] = self.get_parameter('r_camera_angular').value
+        self.R_gps[2, 2] = self.get_parameter('r_gps_angular').value
+
+        self.r_threshold_xy = 1e-3
+        self.r_threshold_theta = 1e-2
 
     def init_topics(self):
         self.create_subscription(PoseWithCovarianceStamped, 'lidar_pose', self.gps_callback, 1)
@@ -130,6 +140,11 @@ class EKFFootprintBroadcaster(Node):
         self.R_gps[0, 0] = msg.pose.covariance[0]    
         self.R_gps[1, 1] = msg.pose.covariance[7]
         self.R_gps[2, 2] = msg.pose.covariance[35]
+        for i in range(2):
+            if self.R_gps[i, i] > self.r_threshold_xy :
+                self.R_gps[i, i] = self.r_threshold_xy 
+        if self.R_gps[2, 2] > self.r_threshold_theta:
+            self.R_gps[2, 2] = self.r_threshold_theta
         self.ekf_update(gps_measurement, self.R_gps)
 
     def camera_callback(self, msg):
@@ -189,9 +204,10 @@ class EKFFootprintBroadcaster(Node):
         self.X[2] += w * dt
         self.footprint_publish()
         self.P = self.P + self.Q
-        # if (self.P[0, 0] > 1) | (self.P[1, 1] > 1 ) | (self.P[2, 2] > 1) :
-        #     self.get_logger().warn(f"large Cov_update:{self.P[0, 0]},{self.P[1, 1]},{self.P[2, 2]}")
-        #     self.P = np.eye(3) * 1e-3
+        if (self.P[0, 0] > 1e-2) | (self.P[1, 1] > 1e-2 ) | (self.P[2, 2] > 0.003) :
+            self.get_logger().warn(f"large Cov_update:{self.P[0, 0]},{self.P[1, 1]},{self.P[2, 2]}")
+            self.P = np.eye(3) * 1e-2
+            self.P[2, 2] = 0.003
 
     def ekf_update(self, z, R):
         if np.any(np.isnan(z)):  # Check if the measurement is valid
@@ -206,10 +222,10 @@ class EKFFootprintBroadcaster(Node):
             residual[2] = normalize_angle(residual[2])
         self.X = self.X + K @ residual
 
-        # if (self.P[0, 0] > 1) | (self.P[1, 1] > 1 ) | (self.P[2, 2] > 1) : # TODO: position and theta should be checked seperately
-        #     self.get_logger().warn(f"large Cov_update:{self.P[0, 0]},{self.P[1, 1]},{self.P[2, 2]}")
-        #     self.P = np.eye(3) * 1e-2
-        #     self.P[2, 2] = 1e-4
+        if (self.P[0, 0] > 1e-2) | (self.P[1, 1] > 1e-2 ) | (self.P[2, 2] > 0.003) : # TODO: position and theta should be checked seperately
+            self.get_logger().warn(f"large Cov_update:{self.P[0, 0]},{self.P[1, 1]},{self.P[2, 2]}")
+            self.P = np.eye(3) * 1e-2
+            self.P[2, 2] = 0.003
             
     def footprint_publish(self):
         t = TransformStamped()
