b72039b5f8929bec6966caf7760aa0585bf0f2b9
jossiewang <jossiew621@gapp.nthu.edu.tw>
2025-04-29 19:58:22 +0800

    Modified: (lidar) likelihood related param

--- localization-devel-ws/global/lidar_localization_pkg/lidar_localization_pkg/lidar_member_function.py
+++ localization-devel-ws/global/lidar_localization_pkg/lidar_localization_pkg/lidar_member_function.py
@@ -142,9 +142,9 @@ class LidarLocalization(Node): # inherit from Node
             orientation += 2 * np.pi
         self.robot_pose = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, orientation])
         self.P_pred = np.array([
-            [msg.pose.covariance[0]*100, 0, 0],
-            [0, msg.pose.covariance[7]*100, 0],
-            [0, 0, msg.pose.covariance[35]*1e6]
+            [msg.pose.covariance[0]*50, 0, 0],
+            [0, msg.pose.covariance[7]*50, 0],
+            [0, 0, msg.pose.covariance[35]*1e4]
         ])
 
     def set_lidar_side_callback(self, msg):
