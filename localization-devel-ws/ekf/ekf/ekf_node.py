fa73879ae1939c043cbe8edec2b7464a0f946f67
jossiewang <jossiew621@gapp.nthu.edu.tw>
2025-05-09 01:27:53 +0800

    Modified:(ekf) fast_spin and refresh area

--- localization-devel-ws/ekf/ekf/ekf_node.py
+++ localization-devel-ws/ekf/ekf/ekf_node.py
@@ -58,7 +58,9 @@ class EKFFootprintBroadcaster(Node):
         self.footprint_publish()
         if self.use_cam:
             self.create_timer(1.0 / self.rate, self.camera_update)
-        
+
+        self.fast_spin = False
+        self.init = False
         
     def claim_parameters(self):
         self.declare_parameter('use_cam', 0)
@@ -71,6 +73,13 @@ class EKFFootprintBroadcaster(Node):
         self.declare_parameter('r_camera_angular', 0.15)
         self.declare_parameter('r_threshold_xy', 1e-3)
         self.declare_parameter('r_threshold_theta', 1e-2)
+        self.declare_parameter('refresh_zone_xl', 0.7)
+        self.declare_parameter('refresh_zone_xr', 2.3)
+        self.declare_parameter('refresh_zone_yl', 0.3)
+        self.declare_parameter('refresh_zone_yr', 1.5)
+        self.declare_parameter('fast_spin_threshold', 3)
+        self.declare_parameter('fast_vx_threshold', 0.5)
+        self.declare_parameter('fast_vy_threshold', 0.5)
         self.parent_frame_id = self.get_parameter('robot_parent_frame_id').value
         self.child_frame_id = self.get_parameter('robot_frame_id').value
         self.rate = self.get_parameter('update_rate').value 
@@ -83,6 +92,13 @@ class EKFFootprintBroadcaster(Node):
         self.use_cam = self.get_parameter('use_cam').value
         self.r_threshold_xy = self.get_parameter('r_threshold_xy').value
         self.r_threshold_theta = self.get_parameter('r_threshold_theta').value
+        self.refresh_zone_xl = self.get_parameter('refresh_zone_xl').value
+        self.refresh_zone_xr = self.get_parameter('refresh_zone_xr').value
+        self.refresh_zone_yl = self.get_parameter('refresh_zone_yl').value
+        self.refresh_zone_yr = self.get_parameter('refresh_zone_yr').value
+        self.fast_spin_threshold = self.get_parameter('fast_spin_threshold').value
+        self.fast_vx_threshold = self.get_parameter('fast_vx_threshold').value
+        self.fast_vy_threshold = self.get_parameter('fast_vy_threshold').value
     def init_topics(self):
         self.create_subscription(PoseWithCovarianceStamped, 'lidar_pose', self.gps_callback, 1)
         self.create_subscription(PoseWithCovarianceStamped, 'initial_pose', self.init_callback,1)
@@ -110,6 +126,8 @@ class EKFFootprintBroadcaster(Node):
                 self.P[2, 2] = msg.pose.covariance[35]
 
     def gps_callback(self, msg):
+        if self.fast_spin:
+            return
         self.gps_time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
         current_time = self.get_clock().now().nanoseconds / 1e9
         if abs(current_time - self.gps_time) > 1.5:
@@ -126,6 +144,12 @@ class EKFFootprintBroadcaster(Node):
         )
 
         gps_measurement = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, theta])
+
+        # check refresh zone
+        if gps_measurement[0] < self.refresh_zone_xl or gps_measurement[0] > self.refresh_zone_xr or gps_measurement[1] < self.refresh_zone_yl or gps_measurement[1] > self.refresh_zone_yr:
+            if self.init:
+                return
+
         self.R_gps[0, 0] = msg.pose.covariance[0]    
         self.R_gps[1, 1] = msg.pose.covariance[7]
         self.R_gps[2, 2] = msg.pose.covariance[35]
@@ -135,6 +159,7 @@ class EKFFootprintBroadcaster(Node):
         if self.R_gps[2, 2] > self.r_threshold_theta:
             self.R_gps[2, 2] = self.r_threshold_theta
         self.ekf_update(gps_measurement, self.R_gps)
+        self.init = True
 
     def camera_callback(self, msg):
         self.cam_time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
@@ -175,6 +200,11 @@ class EKFFootprintBroadcaster(Node):
         v_y = msg.twist.twist.linear.y
         w = msg.twist.twist.angular.z
         self.ekf_predict(v_x, v_y, w, dt) 
+
+        if w > self.fast_spin_threshold or v_x > self.fast_vx_threshold or v_y > self.fast_vy_threshold:
+            self.fast_spin = True
+        else:
+            self.fast_spin = False
 
     def ekf_predict(self, v_x, v_y, w, dt):
         theta = self.X[2]
