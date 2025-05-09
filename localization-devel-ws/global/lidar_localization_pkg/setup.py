4a25b5c178fdc939dd0083ac043802fb657272f4
jossiewang <jossiew621@gapp.nthu.edu.tw>
2025-05-09 16:57:28 +0800

    Fix: (lidar) setup.py for symlink
    notes:
    區域	修改前	修改後	目的
    glob() pattern	*launch.[pxy][yma]*	*.launch, *.py, *.xml	把 .launch 與 .xml 檔都涵蓋，且直觀易讀
    launch_files	直接寫在 data_files 裡	事先組合 launch_files / firmware_launch_files 變數	方便日後新增/除錯
    YAML 抓取	config/*.yml	*.yml + *.yaml	同時支援兩種副檔名
    data_files 目錄	launch/firmware 字串硬寫	os.path.join('share', package_name, 'launch', 'firmware')	保持跨平台與可讀性

--- localization-devel-ws/global/lidar_localization_pkg/setup.py
+++ localization-devel-ws/global/lidar_localization_pkg/setup.py
@@ -4,24 +4,36 @@ from setuptools import setup, find_packages
 
 package_name = 'lidar_localization_pkg'
 
+launch_files = (glob('launch/*.launch') +
+                glob('launch/*.py') +
+                glob('launch/*.xml'))
+
+firmware_launch_files = (glob('launch/firmware/*.launch') +
+                         glob('launch/firmware/*.py') +
+                         glob('launch/firmware/*.xml'))
+
+config_files = glob('config/*.yml') + glob('config/*.yaml')
+
 setup(
     name=package_name,
     version='0.0.0',
-    # Packages to export
     packages=find_packages(exclude=['test']),
-    # Files we want to install, specifically launch files
+
     data_files=[
-        # Install marker file in the package index
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        # Include our package.xml file
-        (os.path.join('share', package_name), ['package.xml']),
-        # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch/firmware', '*launch.[pxy][yma]*'))),
-        # Include all config files.
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yml'))),
+         ['resource/' + package_name]),
+        (os.path.join('share', package_name),
+         ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'),
+         launch_files),
+        (os.path.join('share', package_name, 'launch', 'firmware'),
+         firmware_launch_files),
+
+        (os.path.join('share', package_name, 'config'),
+         config_files),
     ],
+
     install_requires=[
         'setuptools',
         'rclpy',
@@ -48,7 +60,7 @@ setup(
         'console_scripts': [
             'lidar_localization = lidar_localization_pkg.lidar_member_function:main',
             'circle_publisher = lidar_localization_pkg.probability_circle_publisher:main',
-            'pred_publisher = lidar_localization_pkg.pred_publisher:main',
+            'pred_publisher   = lidar_localization_pkg.pred_publisher:main',
         ],
     },
 )
